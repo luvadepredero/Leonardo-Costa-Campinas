774a2ef0cc6e540f92b62be8f373decad214dcbd
luvadepredero <99338809+luvadepredero@users.noreply.github.com>
2022-03-15 15:15:27 -0300

    Add files via upload

---
+++ exercicios aula/exercicio_51.ino
@@ -0,0 +1,22 @@
+const int botao3 = 4;
+const int led1 = 5;
+
+void setup()
+{
+  pinMode(botao3, INPUT);
+  pinMode(led1, OUTPUT);
+  
+}
+
+int valor1;
+
+void loop()
+{
+ valor1 = digitalRead(botao3);
+
+ if(valor1){
+  digitalWrite(led1, LOW);
+ }else{
+  digitalWrite(led1, HIGH);
+ }
+}
